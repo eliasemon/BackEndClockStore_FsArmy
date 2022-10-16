d0a610ca6671a9a2e28cb6c5789305dbca4cde0a
Elias Emon <019eliashossain@gmail.com>
2022-10-17 00:39:50 +0600

    Error Handle

--- config/database.js
+++ config/database.js
@@ -1,14 +1,23 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ClockStore'),
-      user: env('DATABASE_USERNAME', 'ClockStore'),
-      password: env('DATABASE_PASSWORD', 'ClockStore'),
-      ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), },
-      
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'ClockStore'),
+//       user: env('DATABASE_USERNAME', 'ClockStore'),
+//       password: env('DATABASE_PASSWORD', 'ClockStore'),
+//       ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), },
+      
+//     },
+//   },
+// });
