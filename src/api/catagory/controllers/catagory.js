7267632a3fdf1308c20b56e09bc3d3abfe505730
Elias Emon <019eliashossain@gmail.com>
2022-10-24 20:08:25 +0600

    Customize find method for catagory

--- src/api/catagory/controllers/catagory.js
+++ src/api/catagory/controllers/catagory.js
@@ -6,4 +6,28 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::catagory.catagory');
+module.exports = createCoreController('api::catagory.catagory',({strapi})=>({
+    async find(ctx){
+        try {
+            ctx.query = { ...ctx.query, local: 'en' };
+            const { data , meta } = await super.find(ctx);
+            const newData = data.map((data)=>{
+                return {
+                    "id" : data.id,
+                    "name" : data.attributes.name,
+                    "catagoryId":  data?.attributes?.catagoryId,
+                    "sub_catagories" : data.attributes?.sub_catagories?.data.map((data)=>{
+                        return {
+                            "id" : data?.id,
+                            "name" : data?.attributes?.name,
+                            "subCatagoryID":  data?.attributes?.subCatagoryID,
+                        }
+                    })
+                }
+            })
+            return { data : newData , meta}      
+        } catch (error) {
+            ctx.body = error
+        }
+    }
+}));
